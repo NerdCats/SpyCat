bbc3a6e857b03b21256aa1ff12558809735796a1
tareq89 <tareqaziz.aust.cse@gmail.com>
2016-10-02 20:10:00 +0600

    refactored the excel creator funtion

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require('express');
-var excelbuilder = require('msexcel-builder');
 var calculate = require('./calculate');
 var utility = require('./utility');
 var queryMaker = require('./queryMaker');
+var excelCreator = require('./excelCreator');
 var cors = require('cors')
 var app = express();
 app.use(cors());
@@ -49,44 +49,15 @@ router.get('/report', function (req, res) {
 				} else {
 					db.close();
 					if (req.query.generateexcel == "true") {
-						var directoryName = __dirname + "\\excel";
-						var workbookName = Date.now().toString() + 'sample.xlsx';
-						var reportFileName = directoryName + "\\" + workbookName;
-
-					 	var workbook = excelbuilder.createWorkbook(directoryName, workbookName);					 	
-					 	var sheet1 = workbook.createSheet('sheet1', 10, 12);
-					 	//sheet1.set(column, row, value)
-					 	//Object.keys(report.Assets)[0]
-					 	sheet1.set(1, 1, 'Name');
-					 	sheet1.set(2, 1, 'Delivery');
-					 	sheet1.set(3, 1, 'Pending');
-					 	sheet1.set(4, 1, 'InProgress');
-					 	sheet1.set(5, 1, 'Completed');
-					 	sheet1.set(6, 1, 'Cancelled');
-					 	sheet1.set(7, 1, 'ProductPrice');
-					 	sheet1.set(8, 1, 'DeliveryCharge');
-						for (var row = 2; row <= 10; row++) {
-							var reportBaseIndex = row - 2;
-							var key = Object.keys(report)[reportBaseIndex];
-							sheet1.set(1, row, key);
-							sheet1.set(2, row, report[key].TotalDelivery);
-						 	sheet1.set(3, row, report[key].TotalPending);
-						 	sheet1.set(4, row, report[key].TotalInProgress);
-						 	sheet1.set(5, row, report[key].TotalCompleted);
-						 	sheet1.set(6, row, report[key].TotalCancelled);
-						 	sheet1.set(7, row, report[key].TotalProductPrice);
-						 	sheet1.set(8, row, report[key].TotalDeliveryCharge);
-						}
-
-						// Save it 
-						workbook.save(function(ok){
-							if (!ok) {
-								// workbook.cancel();
-								console.log(ok)
-								console.log(reportFileName)
-								res.download(reportFileName);
+						var excelReport = excelCreator.getSummaryReport(report);
+						
+						excelReport.workbook.save(function(ok){
+							console.log(ok)
+							console.log(excelReport.workbookFilePath)
+							if (!ok) {								
+								res.download(excelReport.workbookFilePath);
 							} else							
-								res.json({ date: "congratulations, your workbook created"});
+								res.json({ errorMessage: "couldn't create excel report!"});
 						});						
 					} else {
 						res.json({ data: report });
